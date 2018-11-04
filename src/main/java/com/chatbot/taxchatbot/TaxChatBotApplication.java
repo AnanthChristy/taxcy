fa9f12b2a14ff2ee0f509ad76d1db32ac6065fb3
Bijal Patel <bijal.patel4@gmail.com>
2018-11-03 18:00:58 -0700

    Renamed java file.

--- src/main/java/com/chatbot/taxchatbot/TaxChatBotApplication.java
+++ src/main/java/com/chatbot/taxchatbot/TaxChatBotApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TaxChatbotApplication {
+public class TaxChatBotApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TaxChatbotApplication.class, args);
+		SpringApplication.run(TaxChatBotApplication.class, args);
 	}
 }
