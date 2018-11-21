19432ffd44945266ea4e78d2b5cc6045e4f70565
Erwin <>
2018-11-20 20:27:00 -0800

    put API call inside function

--- src/main/resources/public/taxcy.js
+++ src/main/resources/public/taxcy.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
     $('.next-link').on("click", function(){
         alert($('#title').val() + '. ' + $('#firstname').val() + ' ' + $('#middle-initial').val() + '. ' + $('#lastname').val());
+
+        $.ajax({
+            type: "GET",
+            dataType: "json",
+            url: "https://taxcy.herokuapp.com/chatbot/getAllPropertyInfo/50e1f1a3-e165-4bda-a291-35a024177c33",
+            success: function(data) {
+                alert(data);
+            },
+            error: function() {
+                alert('Error!');
+            }
+        });
     });
 
     /*
@@ -9,16 +21,4 @@ $(document).ready(function() {
         url: "https://taxcy.herokuapp.com/chatbot/personalInfo"
     });
     */
-
-    $.ajax({
-        type: "GET",
-        dataType: "json",
-        url: "https://taxcy.herokuapp.com/chatbot/getAllPropertyInfo/50e1f1a3-e165-4bda-a291-35a024177c33",
-        success: function(data) {
-            alert(data);
-        },
-        error: function() {
-            alert('Error!');
-        }
-    });
 });
